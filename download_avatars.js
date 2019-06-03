cbf52e500cfddae6f56caed610b67a5856a5e8a0
saad hafa <saad.811@icloud.com>
2019-06-03 22:48:48 +0000

    getRepoContributors first step

---
+++ download_avatars.js
@@ -0,0 +1,11 @@
+var request = require('request');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    cb()
+  }
+
+
+  getRepoContributors("jquery", "jquery", function(err, result) {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+  });
