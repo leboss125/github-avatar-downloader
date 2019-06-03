9164338751fbd09ac548b347146322ea8bb89abb
saad hafa <saad.811@icloud.com>
2019-06-03 23:07:35 +0000

    add token and gitignore it then i commit i t

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,18 @@
 var request = require('request');
+var gitToken =  require('./secrets');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-    cb()
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': gitToken.GITHUB_TOKEN
+      }
+    };
+  
+    request(options, function(err, res, body) {
+      cb(err, body);
+    });
   }
 
 
