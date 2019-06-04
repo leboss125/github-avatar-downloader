e734e39184483b390e6944dcd821d662a324f71d
saad hafa <saad.811@icloud.com>
2019-06-04 00:56:11 +0000

    final version

--- download_avatars.js
+++ download_avatars.js
@@ -1,35 +1,40 @@
 // command line to use: node download_avatars.js jquery jquery
 var request = require('request');
-var gitToken =  require('./secrets');
+var gitToken = require('./secrets');
 var arg = process.argv.slice(2);
 var fs = require('fs');
+
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
-      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-      headers: {
-        'User-Agent': 'request',
-        'Authorization': gitToken.GITHUB_TOKEN
-      }
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'User-Agent': 'request',
+            'Authorization': gitToken.GITHUB_TOKEN
+        }
     };
-    request(options, function(err, res,body) {
-        let parsed = JSON.parse(body)
-        cb(parsed)
+    request(options, function (err, res, body) {
+        if(err){
+            console.log('there is an error : ', err);
+        }
+        let parsed = JSON.parse(body);
+        cb(parsed);
     });
-  }
-  function downloadImageByURL(url,path) {
-    request.get(url).on('response',  function(response){
-        response.on('data', function(){
-            console.log('creating files .....')
-        })
-       let result =  response.pipe(fs.createWriteStream(__dirname + path));
-     
-       response.on('end', function(){
-           console.log('all files have been created')
-       })
-    })
-  }
-  getRepoContributors(arg[0], arg[1], function(data) {
-        data.forEach(function(el){
-         downloadImageByURL(el.avatar_url,`/avatars/${el.login}${'.jpeg'}`)
-        })
-  });
+}
+
+function downloadImageByURL(url, path) {
+    request.get(url).on('response', function (response) {
+        response.on('data', function () {
+            console.log('creating files .....');
+        });
+        let result = response.pipe(fs.createWriteStream(__dirname + path));
+
+        response.on('end', function () {
+            console.log('all files have been created');
+        });
+    });
+}
+getRepoContributors(arg[0], arg[1], function (data) {
+    data.forEach(function (el) {
+        downloadImageByURL(el.avatar_url, `/avatars/${el.login}${'.jpeg'}`);
+    });
+});
