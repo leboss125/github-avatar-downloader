31fe7e6740591a0961813816451b3695c62dbcc1
saad hafa <saad.811@icloud.com>
2019-06-04 00:28:33 +0000

    its working but i have to add command line arg

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var gitToken =  require('./secrets');
-
+var fs = require('fs');
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
       url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -9,14 +9,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
         'Authorization': gitToken.GITHUB_TOKEN
       }
     };
-  
-    request(options, function(err, res, body) {
-      cb(err, body);
+    request(options, function(err, res,body) {
+        // cb(JSON.parse(res));
+        
+        let parsed = JSON.parse(body)
+        cb(parsed)
     });
   }
+  function downloadImageByURL(url,path) {
+    request.get(url).on('response',  function(response){
+       let result =  response.pipe(fs.createWriteStream(__dirname + path));
+       request.on('data', function(data){
+           console.log('creating files .....')
+       })
+    })
+  }
 
-
-  getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
+  getRepoContributors("jquery", "jquery", function(data) {
+        data.forEach(function(el){
+         downloadImageByURL(el.avatar_url,`/avatars/${el.login}${'.jpeg'}`)
+        })
   });
